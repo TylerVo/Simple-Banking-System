d4672aa54638c7503be9f9b3be199709d1034039
TylerVo <tyler@umbc.edu>
2016-12-06 21:01:48 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Account
@@ -50,58 +51,69 @@ public class Account
 
         String filename = "accounts.dat";
 
-//Create account
-     if (choice == 2)
-    {
-//prompts user for account details and writes them accordingly
-        try (DataOutputStream output = new DataOutputStream(new FileOutputStream(filename, true));){
+        //Create account
+        if (choice == 2)
+        {
+            //prompts user for account details and writes them accordingly
+            try (DataOutputStream output = new DataOutputStream(new FileOutputStream(filename, true));){
 
-            System.out.println("Enter the user's first name: ");
-            String fName = input.next();
-            fName = fName.toLowerCase();
-            fName += " ";
+                System.out.println("Enter the user's first name: ");
+                String fName = input.next();
+                fName = fName.toLowerCase();
+                fName += " ";
+                if ((fName.length() > (20))) {
+                    System.out.println("First name cannot have more than 20 characters");
+                    create_account();
+                }
+
+                System.out.println("Enter the user's last name: ");
+                String lName = input.next();
+                lName = lName.toLowerCase();
+                lName += " ";
+                if ((lName.length() > (20))) {
+                    System.out.println("Last name cannot have more than 20 characters");
+                    create_account();
+                }
+
+                System.out.println("Enter the user's account number: ");
+                String aNumber = input.next();
+                aNumber += " ";
+                if (!(aNumber.length() == (9))) {
+                    System.out.println("Bank account must be 9 digits long.");
+                    create_account();
+                }
+
+            System.out.println("Please Enter a Number Between 1 and 100");
+
+                System.out.println("Enter the user's account type: ");
+                String aType = input.next();
+                aType = aType.toUpperCase();
+                aType += " ";
 
 
-            System.out.println("Enter the user's last name: ");
-            String lName = input.next();
-            lName = lName.toLowerCase();
-            lName += " ";
+                System.out.println("Enter the user's initial deposit: ");
+                String iDeposit = input.next();
+                iDeposit += " ";
 
+                String write = fName + lName + aNumber + aType + iDeposit;
+                output.writeUTF(write);
+                output.close();
+                Menu.show_menu();
 
-            System.out.println("Enter the user's account number: ");
-            String aNumber = input.next();
-            aNumber += " ";
-
-
-            System.out.println("Enter the user's account type: ");
-            String aType = input.next();
-            aType = aType.toUpperCase();
-            aType += " ";
-
-
-            System.out.println("Enter the user's initial deposit: ");
-            String iDeposit = input.next();
-            iDeposit += " ";
-
-            String write = fName + lName + aNumber + aType + iDeposit;
-            output.writeUTF(write);
-            output.close();
-            Menu.show_menu();
-
+            }
         }
+
+        //Look Up account details
+        else if (choice == 1)
+        {
+            Lookup.lookup_account();
+            Menu.show_menu();
+        }
+
+        input.close();
+
     }
 
-//Look Up account details
-    else if (choice == 1)
-    {
-        Lookup.lookup_account();
-        Menu.show_menu();
-    }
-     
-     input.close();
-     
-    }
-    
 
     public static void deposit (double n)
     {
@@ -127,7 +139,7 @@ public class Account
         System.out.println("Balance: " + balance);
     }
 
-    public static void edit_account ()  throws IOException, InputMismatchException
+    public static void edit_account ()  throws IOException, InputMismatchException, NoSuchElementException
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the old first name:" );
@@ -147,15 +159,15 @@ public class Account
 
         try { // To keep system from crashing.
 
-        	ArrayList<String> data = new ArrayList<String>();
-        	// Opening the stream for reading.
-            DataInputStream stream = new DataInputStream(new FileInputStream("accounts.dat")); 
+            ArrayList<String> data = new ArrayList<String>();
+            // Opening the stream for reading.
+            DataInputStream stream = new DataInputStream(new FileInputStream("accounts.dat"));
             try{ // if it is end of file, then throw exception.
-            	while(true){ // Runs until break.
-                	String temp = stream.readUTF(); // read utf.
-                	if(temp == null) // if data read is null then 
-                		break;
-                	data.add(temp); // else add the string to array list.
+                while(true){ // Runs until break.
+                    String temp = stream.readUTF(); // read utf.
+                    if(temp == null) // if data read is null then
+                        break;
+                    data.add(temp); // else add the string to array list.
                 }
             }catch(Exception e){
             }
@@ -163,43 +175,42 @@ public class Account
 
             boolean found = false; // to check is data found or not.
             // Iterate through all data to check if it contains data or not.
-            for(int i = 0; i < data.size(); i++){ 
-            	if(data.get(i).contains(dataOld)){
-            		found = true;
-            		data.set(i, data.get(i).replace(dataOld, dataNew)); // replace the data.
-            	}
+            for(int i = 0; i < data.size(); i++){
+                if(data.get(i).contains(dataOld)){
+                    found = true;
+                    data.set(i, data.get(i).replace(dataOld, dataNew)); // replace the data.
+                }
             }
-            
+
             if(!found){ // if it is not found then.
-            	System.out.println("Cant't found the user with data: "+dataOld); // print the data.
+                System.out.println("Cant't found the user with data: "+dataOld); // print the data.
             }else{ // re-write the file.
-            	
-            	// open a file.
-                File file = new File("accounts.dat"); 
+
+                // open a file.
+                File file = new File("accounts.dat");
                 if(file.exists())
-                  	file.delete();
-                
+                    file.delete();
+
                 // create a new file and open the output stream.
                 DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(file));
                 for(int i = 0; i < data.size(); i++){
-                	String text = data.get(i);
-                	outputStream.writeUTF(text);
+                    String text = data.get(i);
+                    outputStream.writeUTF(text);
                 }
                 outputStream.close();
-                
+
                 System.out.println("Successfully Updated the Data.");
-            	
+
             }
 
-        } catch (Exception e) {
+        } catch (NoSuchElementException e) {
             System.out.println("Problem reading file.");
         }
-
     }
 
     public static void exit ()
     {
- 
+
         System.out.println("Thank you for using Simple Banking System.");
         System.exit(0);
     }
